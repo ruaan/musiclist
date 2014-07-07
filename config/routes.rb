d6063f44cd022ffac61151f5de5356f08ff2c041
Ruaan Erasmus <eruaan@gmail.com>
2014-07-07 09:29:30 +0200

    database

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Musiclist::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root :to => redirect('/albums')
+  root :to => redirect('/show/index')
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
  # get "pages/info"
